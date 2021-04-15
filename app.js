b016d835821953f1e66712c93647b569bd462a8e
jéssica senger <jessicasbogoni1@gmail.com>
2021-04-15 09:30:43 -0400

    Delete todos

--- app.js
+++ app.js
@@ -22,3 +22,10 @@ addForm.addEventListener("submit", (e) => {
     addForm.reset();
   }
 });
+
+// delete todos
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+  }
+});
