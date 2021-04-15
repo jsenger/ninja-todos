8c94a019b5d045b2839f250232aa787517a015b8
jéssica senger <jessicasbogoni1@gmail.com>
2021-04-15 09:51:56 -0400

    Search todos

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const addForm = document.querySelector(".add");
 const list = document.querySelector(".todos");
+const search = document.querySelector(".search input");
 
+// add todos
 const generateTemplate = (todo) => {
   const html = `
     <li class="list-group-item d-flex justify-content-between align-items-center">
@@ -28,4 +30,20 @@ list.addEventListener("click", (e) => {
   if (e.target.classList.contains("delete")) {
     e.target.parentElement.remove();
   }
+});
+
+// search todos
+const filterTodos = (term) => {
+  Array.from(list.children)
+    .filter((todo) => !todo.textContent.includes(term))
+    .forEach((todo) => todo.classList.add("filtered"));
+
+  Array.from(list.children)
+    .filter((todo) => todo.textContent.includes(term))
+    .forEach((todo) => todo.classList.remove("filtered"));
+};
+
+search.addEventListener("keyup", (e) => {
+  const term = search.value.trim();
+  filterTodos(term);
 });
